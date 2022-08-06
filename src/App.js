221a03672e76587db88c06003fa8434f522622eb
Ishwar <107093882+dev-ishwar@users.noreply.github.com>
2022-08-06 23:44:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,123 @@
+import "./styles.css";
+import "./App.css";
+
+// Checkout App.css for CSS debugging tricks
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Water loading effect</h1>
+      <h1 className="text" data-text="CodeSandbox">
+        CodeSandbox
+      </h1>
+      <div className="ball-box slide-top">
+        <div className="ball1"></div>
+        <div className="ball2"></div>
+        <div className="ball6"></div>
+        <div className="ball3"></div>
+        <div className="ball4"></div>
+        <div className="ball5"></div>
+        <div className="ball1"></div>
+        <div className="ball2"></div>
+        <div className="ball4"></div>
+        <div className="ball5"></div>
+        <div className="ball1"></div>
+        <div className="ball6"></div>
+        <div className="ball2"></div>
+        <div className="ball4"></div>
+        <div className="ball3"></div>
+        <div className="ball1"></div>
+        <div className="ball6"></div>
+        <div className="ball3"></div>
+        <div className="ball2"></div>
+        <div className="ball1"></div>
+        <div className="ball6"></div>
+        <div className="ball4"></div>
+        <div className="ball2"></div>
+        <div className="ball5"></div>
+        <div className="ball1"></div>
+        <div className="ball6"></div>
+        <div className="ball2"></div>
+        <div className="ball3"></div>
+        <div className="ball1"></div>
+        <div className="ball4"></div>
+        <div className="ball5"></div>
+        <div className="ball6"></div>
+        <div className="ball2"></div>
+        <div className="ball3"></div>
+        <div className="ball3"></div>
+        <div className="ball4"></div>
+        <div className="ball1"></div>
+        <div className="ball5"></div>
+        <div className="ball6"></div>
+        <div className="ball2"></div>
+        <div className="ball3"></div>
+        <div className="ball4"></div>
+        <div className="ball5"></div>
+        <div className="ball6"></div>
+        <div className="ball1"></div>
+        <div className="ball2"></div>
+        <div className="ball3"></div>
+        <div className="ball4"></div>
+        <div className="ball5"></div>
+        <div className="ball1"></div>
+        <div className="ball6"></div>
+        <div className="ball2"></div>
+        <div className="ball3"></div>
+        <div className="ball4"></div>
+        <div className="ball5"></div>
+        <div className="ball6"></div>
+        <div className="ball1"></div>
+        <div className="ball2"></div>
+        <div className="ball3"></div>
+        <div className="ball4"></div>
+        <div className="ball5"></div>
+        <div className="ball6"></div>
+        <div className="ball1"></div>
+        <div className="ball2"></div>
+        <div className="ball3"></div>
+        <div className="ball4"></div>
+        <div className="ball5"></div>
+        <div className="ball6"></div>
+      </div>
+
+      <h1>Clip Path: hover effect</h1>
+      <div className="clip"></div>
+
+      <h1>Fancy 🤡 Underline effect </h1>
+      <h4>Wrap text around a shape with shape-outside CSS property</h4>
+      <div className="text-container">
+        <div className="circle"></div>
+
+        <p className="text-content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+          <span className="underline">
+            Temporibus aut veritatis non earum ex, modi, sint dolor illum
+            aperiam commodi neque repudiandae.
+          </span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          voluptas sunt eos rerum molestias assumenda harum illo consectetur
+          voluptatibus? Numquam, sed! Illo aliquid et cum. Rerum cumque
+          obcaecati debitis nulla. Unde tempora itaque consequatur, nesciunt eos
+          rerum sint. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Nulla fuga et eum, fugit accusantium ipsum sunt facere amet omnis!
+          Dolores magnam explicabo dolor fuga cumque vel cupiditate voluptates
+          nostrum optio!
+        </p>
+      </div>
+
+      <h2>Set text inside a circle</h2>
+      <p className="text-in-circle">
+        <span area-hidden="true"></span>
+        Writers write descriptive paragraphs because their purpose is to
+        describe something. Their point is that something is beautiful or
+        disgusting or strangely intriguing. Writers write persuasive and
+        argument paragraphs because their purpose is to persuade or convince
+        someone. Their point is that their reader should see things a particular
+        way and possibly take action on that new way of seeing things. Writers
+        write paragraphs of comparison because the comparison will make their
+        point clear to their readers.
+      </p>
+    </div>
+  );
+}
